e18faf082d72323138fd13cc78181e383cbbfbfe
CandaceRossi <50240571+CandaceRossi@users.noreply.github.com>
2019-08-14 16:31:16 -0400

    took away old files for re-do

--- src/Components/NasaCards.js
+++ src/Components/NasaCards.js
@@ -1,30 +1,31 @@
-import React from "react";
-import styled from "styled-components";
+//Old work
+// import React from "react";
+// import styled from "styled-components";
 
-const CardBg = styled.div`
-  width: 800px;
-  margin: 0 auto;
-  background-image: linear-gradient(to bottom, black, #2c3e50);
-  border-radius: 7px;
-  text-align: center;
-  padding: 10px 10px;
-`;
-const TextStyle = styled.div`
-  color: orange;
-  font-size: 2.3rem;
-  text-shadow: 1px 1px slategray;
-  font-style: sans-serif;
-`;
-function NasaCards(props) {
-  const data = props.data;
-  return (
-    <CardBg>
-      <TextStyle>{data.title}</TextStyle>
-      <img src={data.url} style={{ maxWidth: "450px" }} />
-      <TextStyle>{data.date}</TextStyle>
-      <div>{data.copyright}</div>
-    </CardBg>
-  );
-}
+// const CardBg = styled.div`
+//   width: 800px;
+//   margin: 0 auto;
+//   background-image: linear-gradient(to bottom, black, #2c3e50);
+//   border-radius: 7px;
+//   text-align: center;
+//   padding: 10px 10px;
+// `;
+// const TextStyle = styled.div`
+//   color: orange;
+//   font-size: 2.3rem;
+//   text-shadow: 1px 1px slategray;
+//   font-style: sans-serif;
+// `;
+// function NasaCards(props) {
+//   const data = props.data;
+//   return (
+//     <CardBg>
+//       <TextStyle>{data.title}</TextStyle>
+//       <img src={data.url} style={{ maxWidth: "450px" }} />
+//       <TextStyle>{data.date}</TextStyle>
+//       <div>{data.copyright}</div>
+//     </CardBg>
+//   );
+// }
 
-export default NasaCards;
+// export default NasaCards;
--- src/Components/NasaGrid.js
+++ src/Components/NasaGrid.js
@@ -1,10 +1,11 @@
-import React from "react";
-import NasaCards from "./NasaCards";
-function NasaGrid(props) {
-  return (
-    <div>
-      <NasaCards data={props.displayState} />
-    </div>
-  );
-}
-export default NasaGrid;
+//Old work
+// import React from "react";
+// import NasaCards from "./NasaCards";
+// function NasaGrid(props) {
+//   return (
+//     <div>
+//       <NasaCards data={props.displayState} />
+//     </div>
+//   );
+// }
+// export default NasaGrid;
